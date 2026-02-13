c820b0ddbc7f00f9ac46559f98633884a8d887d8
Rishabh Rao <rishabhrao@users.noreply.github.com>
2026-02-13 18:18:44 +0530

    Update fun_with_flags dependency to point to master branch

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule FunWithFlagsUi.Mixfile do
       {:plug, "~> 1.12"},
       {:plug_cowboy, ">= 2.0.0", optional: true},
       {:cowboy, ">= 2.0.0", optional: true},
-      {:fun_with_flags, git: "https://github.com/invideoio/fun_with_flags.git", branch: "code-style-cleanup"},
+      {:fun_with_flags, git: "https://github.com/invideoio/fun_with_flags.git", branch: "master"},
       {:redix, "~> 1.0", only: [:dev, :test]},
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:credo, "~> 1.7", only: :dev, runtime: false},
